23b100ba1767f57e131d1b68456a77a4681e596d
Jackson Wiebe <jacksonwiebe@me.com>
2019-12-16 23:45:19 -0700

    write new can test, update main code

--- Firmware/CanTest/CanTest.ino
+++ Firmware/CanTest/CanTest.ino
@@ -6,29 +6,97 @@
 
 STM32L4_CAN cancan;
 
-CAN_msg_t derpTX{
-  0x16,
-  {0x080, 0x080, 0x080, 0x080, 0x080, 0x080, 0x08, 0x080},
-  0x08
-};
+//CAN information
+uint32_t lastCANtime;
+uint16_t canRPM;
+uint16_t canFBRAKE;
+uint16_t canFWHEEL;
+uint16_t canRWHEEL;
+uint8_t canThrottle;
+uint8_t canGEAR;
+uint8_t canBRAKELIGHT;
+uint8_t canECUIntervention;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  cancan.CANSend(&derpTX);
-  if (cancan.CANMsgAvail()) {
-    CAN_msg_t derpRX;
-    cancan.CANReceive(&derpRX);
-    Serial.print("Message! ");
-    Serial.println(derpRX.data[2]);
+  CANprocessor();
+  if (millis() % 2000 == 0) {
+    if ((lastCANtime + 1000) <= millis()) {
+      Serial.println("NO CAN");
+    } else {
+      Serial.print("RPM: ");
+      Serial.println(canRPM);
+      Serial.print("canFBRAKE: ");
+      Serial.println(canFBRAKE);
+      Serial.print("canFWHEEL: ");
+      Serial.println(canFWHEEL);
+      Serial.print("canRWHEEL: ");
+      Serial.println(canRWHEEL);
+      Serial.print("canThrottle: ");
+      Serial.println(canThrottle);
+      Serial.print("canGEAR: ");
+      Serial.println(canGEAR);
+      Serial.print("canBRAKELIGHT: ");
+      Serial.println(canBRAKELIGHT, BIN);
+      Serial.print("canECUInter: ");
+      Serial.println(canECUIntervention);
+      Serial.println();
+    }
   }
-  Serial.print("MCR ");
-  Serial.println(CAN1->MCR, BIN);
-  Serial.print("MSR ");
-  Serial.println(CAN1->MSR, BIN);
-  Serial.print("ESR ");
-  Serial.println(CAN1->ESR, BIN);
-  delay(2000);
+}
+
+void CANprocessor() {
+  while (cancan.CANMsgAvail()) {
+    CAN_msg_t canRXmsg;
+    cancan.CANReceive(&canRXmsg);
+    switch (canRXmsg.id) {
+      case 0x120: //RPM
+        if (canRXmsg.len == 8) {
+          canRPM = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          canThrottle = canRXmsg.data[2];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x129: //Gear
+        if (canRXmsg.len == 8) {
+          canGEAR = canRXmsg.data[0];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x290: //Front Brake
+        if (canRXmsg.len == 8) {
+          canFBRAKE = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x12b: //Wheel Speeds
+        if (canRXmsg.len == 8) {
+          canFWHEEL = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          canRWHEEL = (canRXmsg.data[2] << 8) | canRXmsg.data[3];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x450: //ABS MTC MODE change or intervention
+        if (canRXmsg.len == 8) {
+          byte interventionAdder;
+          for (int i = 0; i <= 7; i++) {
+            interventionAdder &= canRXmsg.data[i];
+          }
+          if (interventionAdder != 0) {
+            canECUIntervention = interventionAdder;
+          }
+          lastCANtime = millis();
+        }
+        break;
+      case 0x550: //Brake light
+        if (canRXmsg.len == 8) {
+          canBRAKELIGHT = canRXmsg.data[5];
+          lastCANtime = millis();
+        }
+        break;
+    }
+  }
 }
--- Firmware/FirmwareV01/FirmwareV01.ino
+++ Firmware/FirmwareV01/FirmwareV01.ino
@@ -1,47 +1,44 @@
 #include <PID_v1.h>
 #include <Bounce2.h>
-#include <elapsedMillis.h>
 #include <EEPROM.h>
+#include <STM32L4_CAN.h>
 
 //Pins
 const int pinAnalogA = 0;
 const int pinAnalogB = 1;
 const int pinOutputA = PIN_DAC0;
 const int pinOutputB = PIN_DAC1;
-const int pinRedLED = 3;
-const int pinGreenLED = 4;
-const int pinBlueLED = 5;
+const int pinRedLED = 30;
+const int pinGreenLED = 10;
+const int pinBlueLED = 8;
+const int pinButtonUP = 23;
+const int pinButtonDOWN = 24;
+const int pinButtonMID = 22;
 
-//buttons
+//Buttons
 Bounce debouncerUP = Bounce();
 Bounce debouncerDOWN = Bounce();
-Bounce debouncerCENTRE = Bounce();
+Bounce debouncerMID = Bounce();
 
-//Tunable variables
-const uint32_t maxRPM = 0x5000;
-double Kp = 2, Ki = 5, Kd = 1; //Cruise PID
-
-//CAN Codes, to be confirmed
-const int canHEXIDRPMandThrottle = 0x120;
-const int canHEXIDGear = 0x129; //Check for clutch status here
-const int canHEXIDFBrake = 0x290;
-const int canHEXIDWheelSpeed = 0x12b;
-const int canHEXIDLights = 0x550;
-const int canHEXIDNannyIntervention = 0x450;
+//Can
+STM32L4_CAN cancan;
+//CAN information
+uint32_t lastCANtime;
+uint16_t canRPM;
+uint16_t canFBRAKE;
+uint16_t canFWHEEL;
+uint16_t canRWHEEL;
+uint8_t canThrottle;
+uint8_t canGEAR;
+uint8_t canBRAKELIGHT;
+uint8_t canECUIntervention;
 
 //Globals
 int error = 0;
-boolean cruiseEnabled;
-double cruiseSetSpeed;
-double wheelSpeed;
-double inputThrottle;
-double outputThrottle;
-uint32_t inputA;
-uint32_t inputB;
-uint32_t outputA;
-uint32_t outputB;
 
 //PID
+double PIDwheelSpeed, PIDoutputThrottle, PIDcruiseSetSpeed;
+double Kp = 5, Ki = 1, Kd = 0; //Cruise PID
 PID cruisePID(&wheelSpeed, &outputThrottle, &cruiseSetSpeed, Kp, Ki, Kd, DIRECT);
 
 //EEPROM
@@ -54,34 +51,34 @@ struct configurationFile {
 } currentConfig;
 
 void setup() {
+  Serial.begin(9600);
+
   //Pin IO
-  debouncerUP.attach(BUTTON_PIN);
-  debouncerUP.interval(5);
-  debouncerDOWN.attach(BUTTON_PIN);
-  debouncerDOWN.interval(5);
-  debouncerMID.attach(BUTTON_PIN);
-  debouncerMID.interval(5);
+  debouncerUP.attach(pinButtonUP);
+  debouncerUP.interval(6);
+  debouncerDOWN.attach(pinButtonDOWN);
+  debouncerDOWN.interval(6);
+  debouncerMID.attach(pinButtonMID);
+  debouncerMID.interval(6);
   analogReadPeriod(2); //2 is default, can be lengthened for smoothing
   analogReadResolution(12);
   analogWriteResolution(12);
 
-  //Comms
-  Serial.begin(9600);
-
   //Math
   cruisePID.SetMode(AUTOMATIC);
-  cruisePID.SetSampleTime(100);
+  cruisePID.SetSampleTime(10);
 
   //check for setup mode
   debouncerMID.update();
+  delay(5);
+  debouncerMID.update();
   if (debouncerMID.read() == LOW) {
-    elapsedMillis setupTimer;
-    while (debouncerMID.read() == LOW && setupTimer < 5000) {
+    while (debouncerMID.read() == LOW && debouncerMID.duration() < 5000) {
       LEDupdate(setupTimer % 0x3ff, 0, 0); //pulse red
       debouncerMID.update();
       delayMicroseconds(10);
     }
-    if (setupTimer >= 5000) { //pin was held long enough for setup mode
+    if (debouncerMID.duration() >= 5000) { //pin was held long enough for setup mode
       setupMode();
     }
   }
@@ -91,23 +88,45 @@ void setup() {
 }
 
 void loop() {
-  readAnalogs();
-  UIprocessor();
+  //Inputs
+  CANprocessor();
+  readIO();
+  //process information
   Cruiseprocessor();
+  //Outputs
   calcOutputs();
+  //Teritary User Interface
+  UIprocessor();
+}
+
+void Cruiseprocessor() {
+  static boolean cruiseEN = false;
+  if (sanityChecker()) { //Check all the new information is good.
+	PIDwheelSpeed = (double)canRWHEEL;
+	cruisePID.Compute();
+  } else { //something is not right, disable cruise
+    cruiseEN = false;
+  }
+}
+
+boolean sanityChecker() {
+  if (canECUIntervention) { //Some ecu intervention took place
+    canECUIntervention = 0;
+    return 1;
+  }
 }
 
 void configINIT() {
   configurationFile storedConfig;
   EEPROM.get(0, storedConfig);
   if (storedConfig.ConfigDone !=  0x0f7f) { //no configuration set
-    while (1) { //Error, no config set!
+    while (1) {
       LEDupdate(0x3FF, 0, 0);
       delay(500);
       LEDupdate(0, 0, 0);
       delay(500);
     }
-  }else{
+  } else {
     currentConfig = storedConfig; //good to go
   }
 }
@@ -118,51 +137,30 @@ void LEDupdate(uint32_t R, uint32_t G, uint32_t B) {
   analogWrite(pinBlueLED, B & 0x3FF);
 }
 
-void computeCruiseOutputs() {
-  if (inputThrottle > outputThrottle) { //user is accelerating
-    computeNormalOutputs();
-  }
-  else {
-    cruisePID.Compute();
-    outputA = (uint32_t)outputThrottle;
-    outputB = map((uint32_t)outputThrottle, 0, 4095, 4095, 0);
-  }
-}
 
-void computeNormalOutputs() {
-  outputA = inputA;
-  outputB = inputB;
-}
-
-void inputReads() { //Read all the appropriate signals, Check for errors
+void readIO() { //Read all the appropriate signals, Check for errors
+  debouncerUP.update();
+  debouncerMID.update();
+  debouncerDOWN.update();
   inputA = analogRead(pinAnalogA);
   inputB = analogRead(pinAnalogB);
-  if (inputA < minADC) {
-    error = 1;
-  } else if (inputB < minADC) {
-    error = 2;
-  }  else if (inputA > maxADC) {
-    error = 3;
-  }  else if (inputB > maxADC) {
-    error = 4;
-  }
-  inputThrottle = (inputA + map(inputB, 0, 4095, 4095, 0)) / 2; //average the two inputs
 }
 
-void analogWrites() {
+void calcOutputs() {
   analogWrite(pinOutputA, outputA);
   analogWrite(pinOutputB, outputB);
 }
 
-void errorPrinter() {
+void UIprocessor() {
   /* ERROR MESSAGES
     1 Input A too low
     2 Input B too low
     3 Input A too high
     4 Input B too high
+    5 NO CAN
   */
   static uint32_t lastPrint = 0;
-  if (lastPrint  + 1000 < millis()) {
+  if (lastPrint  + 2000 <= millis() && !error) {
     switch (error) {
       case 0:
         if (cruiseEnabled) {
@@ -172,28 +170,89 @@ void errorPrinter() {
         }
         break;
       case 1:
-        RGBwriter(4095, 0, 0); // Red
+        LEDupdate(4095, 0, 0); // Red
         Serial.println("1 Input A too low");
         break;
       case 2:
-        RGBwriter(4095, 0, 0); // Red
+        LEDupdate(4095, 0, 0); // Red
         Serial.println("2 Input B too low");
         break;
       case 3:
-        RGBwriter(4095, 0, 0); // Red
+        LEDupdate(4095, 0, 0); // Red
         Serial.println("3 Input A too high");
         break;
       case 4:
-        RGBwriter(4095, 0, 0); // Red
+        LEDupdate(4095, 0, 0); // Red
         Serial.println("4 Input B too high");
+        break;
+      case 5:
+        LEDupdate(4095, 0, 0); // Red
+        Serial.println("5 NO CAN");
         break;
     }
     lastPrint = millis();
   }
 }
 
-void RGBwriter(int red, int green, int blue) {
-  analogWrite(pinRedLED, red);
-  analogWrite(pinGreenLED, green);
-  analogWrite(pinBlueLED, blue);
+void LEDupdate(uint32_t R, uint32_t G, uint32_t B) {
+  analogWrite(pinRedLED, R % 0x3FF);
+  analogWrite(pinGreenLED, G % 0x3FF);
+  analogWrite(pinBlueLED, B & 0x3FF);
+}
+
+void CANprocessor() {
+  static uint32_t lastCANtime;
+  while (cancan.CANMsgAvail()) {
+    CAN_msg_t canRXmsg;
+    cancan.CANReceive(&canRXmsg);
+    switch (canRXmsg.id) {
+      case 0x120: //RPM
+        if (canRXmsg.len == 8) {
+          canRPM = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          canThrottle = canRXmsg.data[2];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x129: //Gear
+        if (canRXmsg.len == 8) {
+          canGEAR = canRXmsg.data[0];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x290: //Front Brake
+        if (canRXmsg.len == 8) {
+          canFBRAKE = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x12b: //Wheel Speeds
+        if (canRXmsg.len == 8) {
+          canFWHEEL = (canRXmsg.data[0] << 8) | canRXmsg.data[1];
+          canRWHEEL = (canRXmsg.data[2] << 8) | canRXmsg.data[3];
+          lastCANtime = millis();
+        }
+        break;
+      case 0x450: //ABS MTC MODE change or intervention
+        if (canRXmsg.len == 8) {
+          byte interventionAdder;
+          for (int i = 0; i <= 7; i++) {
+            interventionAdder &= canRXmsg.data[i];
+          }
+          if (interventionAdder != 0) {
+            canECUIntervention = interventionAdder;
+          }
+          lastCANtime = millis();
+        }
+        break;
+      case 0x550: //Brake light
+        if (canRXmsg.len == 8) {
+          canBRAKELIGHT = canRXmsg.data[5];
+          lastCANtime = millis();
+        }
+        break;
+    }
+  }
+  if ((lastCANtime + 1000) <= millis()) {
+    error = 5;
+  }
 }
